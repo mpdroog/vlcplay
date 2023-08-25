50b5f9699a5d1afd15e4622bcee6d1f88948dce9
mark <mpdroog@rootdev.nl>
2023-08-25 18:22:58 +0200

    Feature. Random play songs and basic control from CLI

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"log"
-
+	"bufio"
+	"fmt"
 	vlc "github.com/adrg/libvlc-go/v3"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -31,27 +36,37 @@ func main() {
 	}
 	defer list.Release()
 
-	err = list.AddMediaFromPath("/Volumes/MPExtern/Music")
-	if err != nil {
-		log.Fatal(err)
+	songs := make(map[string]struct{})
+	if e := filepath.WalkDir("/Volumes/MPExtern/Music", func(s string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		if !strings.HasSuffix(s, ".mp4") {
+			return nil
+		}
+
+		//fmt.Printf("Read %s\n", s)
+		songs[s] = struct{}{}
+		return nil
+
+	}); e != nil {
+		log.Fatal(e)
 	}
 
-	/*err = list.AddMediaFromURL("http://stream-uk1.radioparadise.com/mp3-32")
-	if err != nil {
-		log.Fatal(err)
-	}*/
+	// Add in second range by 'abusing' random behaviour of map
+	for song, _ := range songs {
+		if err := list.AddMediaFromPath(song); err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	// Set player media list.
 	if err = player.SetMediaList(list); err != nil {
 		log.Fatal(err)
 	}
-
-	/* Media files can be added to the list after the list has been added
-	// to the player. The player will play these files as well.
-	err = list.AddMediaFromPath("localpath/test2.mp3")
-	if err != nil {
-		log.Fatal(err)
-	}*/
 
 	// Retrieve player event manager.
 	manager, err := player.EventManager()
@@ -71,11 +86,58 @@ func main() {
 	}
 	defer manager.Detach(eventID)
 
+	// Show song we're playing
+	{
+		_, err := manager.Attach(vlc.MediaPlayerPlaying, func(event vlc.Event, userData interface{}) {
+			fmt.Printf("%+v\n", userData)
+		}, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// todo defer manager.Detach(eventID)
+	}
+
+	if err := player.SetPlaybackMode(vlc.Loop); err != nil {
+		log.Fatal(err)
+	}
+
 	// Start playing the media list.
 	if err = player.Play(); err != nil {
 		log.Fatal(err)
 	}
 
+	go func() {
+		cmds := map[string]string{"n": "Next song", "p": "previous song", "t": "Pause or resume"}
+		for {
+			reader := bufio.NewReader(os.Stdin)
+			line, e := reader.ReadString('\n')
+			if e != nil {
+				log.Fatal(err)
+			}
+			//fmt.Println("Line=" + line)
+			cmd := strings.TrimSpace(line)
+			if cmd == "n" {
+				if err := player.PlayNext(); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("Next\n")
+			} else if cmd == "p" {
+				if err := player.PlayPrevious(); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("Prev\n")
+			} else if cmd == "t" {
+				if err := player.TogglePause(); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("Toggle pause\n")
+			} else {
+				fmt.Printf("Unsupported cmd, possible=\n")
+				fmt.Printf("%+v\n", cmds)
+			}
+		}
+
+	}()
+
 	<-quit
 }
-
